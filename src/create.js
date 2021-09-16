2ef0bf94cc216c918a4c3480914a3313d3b3f54e
subhasis020299 <subhasisdas125@gmail.com>
2021-09-16 14:17:33 +0530

    trying with return values

--- src/create.js
+++ src/create.js
@@ -17,6 +17,7 @@ exports.handler = async (event) => {
 			createdAt: timestamp,
 			updatedAt: timestamp,
 		},
+		ReturnValues: "ALL_NEW",
 	};
 	try {
 		const data = await client.put(params).promise();
