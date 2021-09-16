19d97b5e639346dd028e52218ad298deb742461c
subhasis020299 <subhasisdas125@gmail.com>
2021-09-16 13:42:04 +0530

    added logging, better packaging

--- src/create.js
+++ src/create.js
@@ -19,9 +19,10 @@ exports.handler = async (event) => {
 	try {
 		const data = await client.put(params);
 		const serializedData = JSON.parse(data.Attributes);
+		console.log(serializedData);
 		return new Response(200, JSON.stringify(serializedData));
 	} catch (error) {
 		console.error("Error in create", error);
-		return new Response(500, error);
+		return new Response(500, error.message);
 	}
 };
