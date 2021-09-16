d75b5077ee8495c854fdf0fb483e07b4e80980c8
subhasis020299 <subhasisdas125@gmail.com>
2021-09-16 22:13:08 +0530

    sorted data returned

--- src/list.js
+++ src/list.js
@@ -6,6 +6,15 @@ const TableName = process.env.DYNAMODB_TABLE;
 exports.handler = async (event) => {
 	try {
 		let data = await db.list(TableName);
+
+		//sort data in descending order of createdAt
+		data = data.sort((a, b) => {
+			let aDate = new Date(a.createdAt);
+			let bDate = new Date(b.createdAt);
+
+			return bDate.getTime() - aDate.getTime();
+		});
+
 		return Responses._200({ message: "List of items", data });
 	} catch (error) {
 		console.error("Error in listing items", error);
